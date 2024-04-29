800c2b45c1509303202394e33d1c7a804dadccb5
filippo16 <filippo.lutterotti2@gmail.com>
2024-04-29 17:14:54 +0200

    Add entity model in game api

---
+++ src/main/java/it/unibo/dragondash/model/api/game/Entity.java
@@ -0,0 +1,13 @@
+package it.unibo.dragondash.model.api.game;
+
+public abstract class Entity {
+    protected  float x,y;
+    protected int width, height;
+
+    public Entity(float x, float y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
