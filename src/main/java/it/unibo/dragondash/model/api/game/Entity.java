f0a97a66afce252d81163ec92718a8d74436d163
Tommaso Bagnolini <tommaso.bagnolini@studio.unibo.it>
2024-05-02 09:21:39 +0200

    Created Entity class in model

---
+++ src/main/java/it/unibo/dragondash/model/api/game/Entity.java
@@ -0,0 +1,13 @@
+package main.java.it.unibo.dragondash.model.api.game;
+
+public abstract class Entity {
+    protected  float x,y;
+    protected int width, height;
+
+    public Entity(float x, float y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
