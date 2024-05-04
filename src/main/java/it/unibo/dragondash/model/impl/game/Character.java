a8ea575badbdc18e0359a470ba24fa3055a905c3
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-05-04 16:16:55 +0200

    Declaration of the method to set the default values for the character

--- src/main/java/it/unibo/dragondash/model/impl/game/Character.java
+++ src/main/java/it/unibo/dragondash/model/impl/game/Character.java
@@ -14,6 +14,18 @@ public class Character extends Entity{
     public Character(float x, float y, int width, int height) {
         super(x, y, width, height);
         
+        setDefaultValues();
     }
     
+    public void setDefaultValues() {
+        x = 100;
+        y = 300;
+
+        isJumping = false;
+        jumpSpeed = 8;
+        jumpHeight = 300;
+
+        gravity = 2;
+        maxHeight = 290;
+    }
 }
