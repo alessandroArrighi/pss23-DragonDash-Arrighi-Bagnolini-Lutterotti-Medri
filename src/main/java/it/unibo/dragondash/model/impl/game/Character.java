4bc3cdbd2b76bfc1d70ace61559bfcc95818d803
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-05-04 16:14:16 +0200

    Creation of Character class

---
+++ src/main/java/it/unibo/dragondash/model/impl/game/Character.java
@@ -0,0 +1,12 @@
+package it.unibo.dragondash.model.impl.game;
+
+import it.unibo.dragondash.model.api.game.Entity;
+
+public class Character extends Entity{
+
+    public Character(float x, float y, int width, int height) {
+        super(x, y, width, height);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
