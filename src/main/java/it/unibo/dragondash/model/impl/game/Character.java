0545cc484c5222825e3da7f11d407d33212882ff
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-05-04 16:15:22 +0200

    Definition of Character main variables

--- src/main/java/it/unibo/dragondash/model/impl/game/Character.java
+++ src/main/java/it/unibo/dragondash/model/impl/game/Character.java
@@ -4,9 +4,16 @@ import it.unibo.dragondash.model.api.game.Entity;
 
 public class Character extends Entity{
 
+    boolean isJumping;
+    int jumpSpeed;
+    int jumpHeight;
+
+    int gravity;
+    int maxHeight;
+
     public Character(float x, float y, int width, int height) {
         super(x, y, width, height);
-        //TODO Auto-generated constructor stub
+        
     }
     
 }
