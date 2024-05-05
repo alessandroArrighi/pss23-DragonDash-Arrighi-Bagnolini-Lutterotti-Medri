5f8fd8a246eb5c28e19283a7330a3e61a96c4097
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2024-05-05 11:10:16 +0200

    add start and stop methods in MapTemplate

--- src/main/java/it/unibo/dragondash/model/api/game/MapTemplate.java
+++ src/main/java/it/unibo/dragondash/model/api/game/MapTemplate.java
@@ -1,6 +1,7 @@
 package it.unibo.dragondash.model.api.game;
 
 import org.apache.commons.lang3.time.StopWatch;
+import java.lang.IllegalStateException;
 
 public abstract class MapTemplate {
     
@@ -11,4 +12,23 @@ public abstract class MapTemplate {
         this.speed = speed;
         this.state = false;
     }
+
+    public void start() {
+        if(!this.state) {
+            this.state = true;
+        }
+        else {
+            throw new IllegalStateException("Map is already started");
+        }
+    }
+
+    public void stop() {
+        if(this.state) {
+            this.state = false;
+        }
+        else {
+            throw new IllegalStateException("Map is already stopped");
+        }
+    }
+
 }
