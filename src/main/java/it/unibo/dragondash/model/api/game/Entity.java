731a314fca467e7f5b3a512d206c9e56a387aacc
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-05-07 18:42:06 +0200

    Add sprite declaration and import for all entities

--- src/main/java/it/unibo/dragondash/model/api/game/Entity.java
+++ src/main/java/it/unibo/dragondash/model/api/game/Entity.java
@@ -1,7 +1,12 @@
 package it.unibo.dragondash.model.api.game;
 
+import it.unibo.dragondash.view.api.GameView.Sprite;
+
 public abstract class Entity {
     protected  float x,y;
+
+    public Sprite sprite;
+    
     protected int width, height;
 
     public Entity(float x, float y, int width, int height) {
