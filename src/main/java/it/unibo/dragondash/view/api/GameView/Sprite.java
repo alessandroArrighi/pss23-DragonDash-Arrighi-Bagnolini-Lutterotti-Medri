6e5df7aa93fb6fba5bc6c5501ebc5eef06d48b9a
acetheberliner <tommaso.bagnolini@studio.unibo.it>
2024-05-07 18:38:02 +0200

    Definition of constructor and basic Get functions

--- src/main/java/it/unibo/dragondash/view/api/GameView/Sprite.java
+++ src/main/java/it/unibo/dragondash/view/api/GameView/Sprite.java
@@ -5,4 +5,17 @@ import java.awt.*;
 public class Sprite {
     
     public Image image;
+
+    public Sprite(Image image) {
+        this.image = image;
+    }
+
+    public int getWidth() {
+        return image.getWidth(null);
+    }
+
+    public int getHeight() {
+
+        return image.getHeight(null);
+    }
 }
