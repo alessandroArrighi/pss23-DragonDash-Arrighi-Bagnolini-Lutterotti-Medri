df68e133bcaea8589bd8eb3acae7f02d19fd11e1
filippo16 <filippo.lutterotti2@gmail.com>
2024-05-10 16:25:48 +0200

    Add time watch in RectangleModel

--- src/main/java/it/unibo/dragondash/model/api/game/RectangleModel.java
+++ src/main/java/it/unibo/dragondash/model/api/game/RectangleModel.java
@@ -1,9 +1,17 @@
 package it.unibo.dragondash.model.api.game;
 
+import org.apache.commons.lang3.time.StopWatch;
+
 public abstract class RectangleModel extends Entity {
+
+    private StopWatch watch;
 
     public RectangleModel(float x, float y, int width, int height) {
         super(x, y, width, height);
+    }
+
+    protected void getWatch(final StopWatch w) {
+        this.watch = w;
     }
 
     protected abstract void setSpeed();
